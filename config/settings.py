3556efabfcc742fd5bcb39eb9870cd075bdc7474
Christian DIMEMOU <christiandimemou@gmail.com>
2026-08-24 15:13:08 +0100

    je viens de créer le fichier .env

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$5#dstq&@1_n0v^+jk9obgl^20%7f@*t-*^@mwmir78igg83#3'
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-!@#%$^&*()_+1234567890")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[''], cast=lambda v: [s.strip() for s in v.split(',') if s.strip()])
 
 
 # Application definition
